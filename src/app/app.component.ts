d1165afe7750cd03bd25803ce9049f3d6f68dc1b
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-11-01 14:18:38 +0500

    updatd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,7 @@ import { TokenSalesService } from './frontend/staking-pool/token-sales.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit, AfterViewInit {
   constructor(
